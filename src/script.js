e3e543e75edd6469b20d13aa345ea42e8cf7092a
smarranda@gmail.com <smarranda@gmail.com>
2025-06-18 11:22:12 +0200

    lämna in

--- src/script.js
+++ src/script.js
@@ -26,22 +26,20 @@ const contactBook = {
 };
   
   // Ange en grupp
-  let groupToShow;
+  let groupToShow; "familj";
   
 
 
 
-function showContacts(contactBook, groupToShow) { // Rör ej denna kod
+function showContacts(contactBook, groupToShow) {
+    for (const contact of contactBook.contacts) {
+        if (contact.group === groupToShow) {
+            return `Namn: ${contact.name}, Telefon: ${contact.phone}`;
+        }
+    }
 
-
-
-    // Loopar igenom alla kontakter och jämför grupp
-   
-    return  // här ska sträng med namn och tekefonnummer visas i form av string interpolation
-
-
+    return "";
 }
-
 
 // Rör ej denna kod
 showContacts(contactBook, groupToShow);
